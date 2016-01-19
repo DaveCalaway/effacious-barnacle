32271d37542ac34b76f5415b39c880287b5a7c8b
DaveCalaway <davide.gariselli@gmail.com>
2016-01-19 11:41:55 +0100

    update

--- Line_Follower/Line_Follower_shield/Line_Follower_shield.ino
+++ Line_Follower/Line_Follower_shield/Line_Follower_shield.ino
@@ -1,10 +1,10 @@
-/*Version 0.2
+ /*Version 0.4
  * 
  * Library referece: https://goo.gl/D9dSPh
  * Line Follower shield , with high impedance state "RELEASE".
  * Sensor's eyes: 
- *                0 = black-line
- *                1 = white 
+ *                1 = black-line - led spento
+ *                0 = white - led acceso
  * Led on for panic!
  * 
  * DaveCalaway
@@ -21,11 +21,11 @@ int senSX, senCX, senDX = 0;
 void readsen();
 
 void setup() {
-  pinMode(A1,INPUT);  // senSX
-  pinMode(A2,INPUT);  // senCX
-  pinMode(A3,INPUT);  // senDX
-  m_SX.setSpeed(200);     // set the speed to 200/255
-  m_DX.setSpeed(200);
+  pinMode(A0,INPUT);  // senSX
+  pinMode(A1,INPUT);  // senCX
+  pinMode(A2,INPUT);  // senDX
+  m_SX.setSpeed(70);     // set the speed to 200/255
+  m_DX.setSpeed(70);
   Serial.begin(9600);
 }
 
@@ -33,20 +33,25 @@ void loop() {
  
   readsen();
   digitalWrite(led,LOW);
-  
-  // AVANTI
-    //analogWrite(runSX,pwm);
-    m_SX.run(FORWARD);
-    //analogWrite(runDX,pwm);
-    m_DX.run(FORWARD);
-    Serial.println("Dritto");
-    delay(100);
+
+   // RIGA A DESTRA
+  if(senSX == 0 && senCX == 0 && senDX == 1){
+    Serial.println("destra");
+    // rimettimi al centro
+    while(senCX!=1){
+      //analogWrite(runSX,pwm);
+      m_SX.run(FORWARD);
+      //digitalWrite(runDX,LOW);
+      m_DX.run(RELEASE);
+      readsen();
+    } 
+  }   
   
   // RIGA A SINISTRA
-  if(senSX == 0 && senCX == 1 && senDX == 1){
+  if(senSX == 1 && senCX == 0 && senDX == 0){
     Serial.println("sinistra");
     // rimettimi al centro
-    while(senCX!=0){ 
+    while(senCX!=1){ 
       //analogWrite(runDX,pwm);
       m_DX.run(FORWARD);
       //digitalWrite(runSX,LOW);
@@ -54,36 +59,32 @@ void loop() {
       readsen();
     }
   }
-  
-  // RIGA A DESTRA
-  if(senSX == 1 && senCX == 1 && senDX == 0){
-    Serial.println("destra");
-    // rimettimi al centro
-    while(senCX!=0){
-      //analogWrite(runSX,pwm);
-      m_SX.run(FORWARD);
-      //digitalWrite(runDX,LOW);
-      m_DX.run(RELEASE);
-      readsen();
-    }    
-  }
-  
+
   // TUTTO NERO
-  if(senSX == 0 && senCX == 0 && senDX == 0){
+  if(senSX == 1 && senCX == 1 && senDX == 1){
     //digitalWrite(runSX,LOW);
     m_SX.run(RELEASE);
     //digitalWrite(runDX,LOW);
     m_DX.run(RELEASE);
     digitalWrite(led,HIGH);
     Serial.println("dove sono?");
-    delay(1000);
+    delay(2000);
   }
-  
+
+    // AVANTI
+  else{
+    //analogWrite(runSX,pwm);
+    m_SX.run(FORWARD);
+    //analogWrite(runDX,pwm);
+    m_DX.run(FORWARD);
+    Serial.println("Dritto");
+  }
 }
 
  // read for a sensors
 void readsen(){
-  senSX = digitalRead(10);
-  senCX = digitalRead(11);
-  senDX = digitalRead(12);
+  senSX = digitalRead(A0);
+  senCX = digitalRead(A1);
+  senDX = digitalRead(A2);
 }
+
